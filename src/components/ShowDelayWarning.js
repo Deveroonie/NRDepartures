e34265400993b398c5232acd21fb9ef37955f0e6
Deveroonie <sirepiclydev@gmail.com>
2023-11-20 08:01:40 +0000

    GA on Delay warning

--- src/components/ShowDelayWarning.js
+++ src/components/ShowDelayWarning.js
@@ -1,5 +1,6 @@
 import React, {useState,useEffect} from "react";
 import axios from "axios";
+import ReactGA4 from 'react-ga4';
 
 export default function ShowDelayWarning(data) {
   const [response, setResponse] = useState([]);
@@ -8,14 +9,33 @@ export default function ShowDelayWarning(data) {
         try {
           const response = await axios.get(`https://nrdeparturesdelaysapi.cyclic.app/api/v1/${data.type}/${data.stname}?limit=${data.limit}`)
           //const response = await axios.get(`http://localhost:5000/api/v1/${data.type}/${data.stname}?limit=${data.limit}`)
-
+          ReactGA4.send({
+            hitType: 'event',
+            eventCategory: ``,
+            eventAction: 'Show Delay Warning',
+            eventLabel: ``,
+            stationViewed: data.stname,
+            type: data.type,
+            limit: data.limit || 10,
+            services: response.data.services,
+            delayedServices: response.data.delayedServices,
+            onTimeServices: response.data.onTimeServices,
+            operators: response.data.operators,
+            delayedOperators: response.data.delayedOperators,
+            mostServicesDelayed: response.data.mostServicesDelayed,
+            areOperatorsDelayed: response.data.areOperatorsDelayed
+          });
           setResponse(response.data);
         } catch (error) {
           console.error("Error fetching data:", error);
         }
       }
-  
+      async function sendToGA() {
+        
+      }
+      
       fetchData();
+      sendToGA();
     }, []);
     if(response.mostServicesDelayed && !response.areOperatorsDelayed) { // most trains delayed: yes, operator delayed: no
       return (
