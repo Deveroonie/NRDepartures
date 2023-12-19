4c5496f7b5e9ed8e6403ee507ef6261fccb8bd0d
Deveroonie <sirepiclydev@gmail.com>
2023-12-19 22:37:09 +0000

    'this train has been delayed by X minutes' message / delay repay popup upon clicking it

--- src/components/GetBadge.js
+++ src/components/GetBadge.js
@@ -1,6 +1,44 @@
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
+
 export default function GetBadge(data) {
+    const swal = withReactContent(Swal)
         if(data.std !== data.etd && data.etd !== "Cancelled" && data.etd !== "On time") {
-            return (<span className="bg-yellow-500 text-white text-xs font-medium mr-2 px-2.5 py-0.5 rounded ">{data.etd}</span>)
+            if(data.etd != "Delayed") {
+                let delay = data.std.replace(":", "") - data.etd.replace(":", "")
+                return (
+                    <div>
+                    <span className="bg-yellow-500 text-white text-xs font-medium mr-2 px-2.5 py-0.5 rounded">
+                        {data.etd}
+                    </span>
+                    <br />
+                    <span title="Click me to learn more about claiming Delay Repay for this train." onClick={canGetDelayRepayed}>This train has been delayed by {delay.toString().replace("-", "")} minutes</span>
+                    </div>
+                )
+                function canGetDelayRepayed() {
+                    if(delay >= 15) {
+                        swal.fire({
+                            icon: "success",
+                            title: "You should be able to get delay repay for this journey.",
+                            html: "Please see your train company's website for more information."
+                          })
+                    } else {
+                        swal.fire({
+                            icon: "error",
+                            title: "You probably won't be able to get delay repay for this journey.",
+                            html: "Unfortunatley as your train hasn't been delayed by at least 15 minutes you cannot claim delay repay for this journey."
+                          })
+                    }
+
+                }
+            } else {
+            return (
+                <span className="bg-yellow-500 text-white text-xs font-medium mr-2 px-2.5 py-0.5 rounded">
+                    {data.etd}
+                </span>
+            )
+            }
+            
         } else if (data.etd === "Cancelled") {
             return (<span className="bg-red-500 text-white text-xs font-medium mr-2 px-2.5 py-0.5 rounded ">Cancelled</span>)
         } else if(data.etd === "On time") {
